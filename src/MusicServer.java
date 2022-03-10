2f21ed2be7466e16585174428242fca7e75145c1
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2022-03-11 00:42:26 +0300

    build: server refactor

--- src/MusicServer.java
+++ src/MusicServer.java
@@ -5,66 +5,55 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class MusicServer {
-
-    ArrayList<ObjectOutputStream> clientOutputStreams;
+public class MusicServer
+{
+    ArrayList clientOutputStreams;
 
     public static void main(String[] args) {
         new MusicServer().go();
     }
 
     public class ClientHandler implements Runnable {
-
         ObjectInputStream in;
-        Socket clientSocket;
+        Socket sock;
 
-        public ClientHandler(Socket socket) {
+        public ClientHandler(Socket clientSOcket) {
             try {
-                clientSocket = socket;
-                in = new ObjectInputStream(clientSocket.getInputStream());
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+                sock = clientSOcket;
+                in = new ObjectInputStream(sock.getInputStream());
+
+            } catch (Exception ex) { ex.printStackTrace(); }
         }
 
         public void run() {
-            Object o2 = null;
-            Object o1 = null;
+            Object o1;
+            Object o2;
             try {
-
                 while ((o1 = in.readObject()) != null) {
-
                     o2 = in.readObject();
-                    System.out.println("read 2 objects");
+                    System.out.println("read two objects");
                     tellEveryone(o1, o2);
                 }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            } catch (Exception ex) { ex.printStackTrace(); }
         }
-
     }
 
-    public void go() {
-        clientOutputStreams = new ArrayList<ObjectOutputStream>();
 
+
+    public void go() {
+        clientOutputStreams = new ArrayList();
         try {
             ServerSocket serverSock = new ServerSocket(4242);
-
-            while (true) {
-
+            while(true) {
                 Socket clientSocket = serverSock.accept();
                 ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
                 clientOutputStreams.add(out);
 
                 Thread t = new Thread(new ClientHandler(clientSocket));
                 t.start();
-
                 System.out.println("got a connection");
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        } catch (Exception ex) { ex.printStackTrace(); }
     }
 
     public void tellEveryone(Object one, Object two) {
@@ -74,9 +63,7 @@ public class MusicServer {
                 ObjectOutputStream out = (ObjectOutputStream) it.next();
                 out.writeObject(one);
                 out.writeObject(two);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            } catch (Exception ex) { ex.printStackTrace(); }
         }
     }
 }
